e9950f4a0557447adbdf1eeacbd658a173e88da5
Mario Cooper <mariocooper00@hotmail.com>
2022-12-21 22:11:26 +0000

    Isolating with spies

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -5,14 +5,24 @@ const Itinerary = require('../src/Itinerary.js');
 
 describe('Ship', () => {
   describe('with ports and an itinerary', () => {
-    let ship;
     let dover;
     let calais;
-    let itinerary;
-
+    
     beforeEach(() => {
-      dover = new Port('Dover');
-      calais = new Port('Calais');
+      dover = {
+        addShip: jest.fn(),
+        removeShip: jest.fn(),
+        name: 'Dover',
+        ships: []
+      };
+    
+      calais = {
+        addShip: jest.fn(),
+        removeShip: jest.fn(),
+        name: 'Calais',
+        ships: []
+      };
+    
       itinerary = new Itinerary([dover, calais]);
       ship = new Ship(itinerary);
     });
@@ -27,9 +37,9 @@ describe('Ship', () => {
 
     it('can set sail', () => {
       ship.setSail();
-      
+    
       expect(ship.currentPort).toBeFalsy();
-      expect(dover.ships).not.toContain(ship);
+      expect(dover.removeShip).toHaveBeenCalledWith(ship);
     });
 
     it('can dock at a different port', () => {
@@ -37,7 +47,7 @@ describe('Ship', () => {
       ship.dock();
       
       expect(ship.currentPort).toBe(calais);
-      expect(calais.ships).toContain(ship);
+      expect(calais.addShip).toHaveBeenCalledWith(ship);
       });
 
     it('can\'t sail further than its itinerary', () => {
@@ -48,7 +58,7 @@ describe('Ship', () => {
     });
     
     it('gets added to port on instantiation', () => {
-      expect(dover.ships).toContain(ship);
+      expect(dover.addShip).toHaveBeenCalledWith(ship);
     });
   });
 });
