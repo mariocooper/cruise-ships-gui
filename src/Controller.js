38a98d2cd1c021864f765a03332bf0e4b9b63349
Mario Cooper <mariocooper00@hotmail.com>
2023-08-06 15:03:11 +0100

    setSail method added to Controller constructor

--- src/Controller.js
+++ src/Controller.js
@@ -1,6 +1,10 @@
 (function exportController() {
-  function Controller() {      
+  function Controller(ship) {      
+    this.ship = ship;
     this.initialiseSea();
+    document.querySelector('#sailbutton').addEventListener('click', () => {
+      this.setSail();
+    })
   }
 
   Controller.prototype = {
@@ -30,13 +34,36 @@
       })
     },
 
-    renderShip(ship) {
+    renderShip() {
+      const ship = this.ship;
       const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
       const portElement = document.querySelector(`[data-port-index='${shipPortIndex}']`);
       const shipElement = document.querySelector('#ship');
       shipElement.style.top = `${portElement.offsetTop + 32}px`;
       shipElement.style.left = `${portElement.offsetLeft + 32}px`;
+    },
 
+    setSail() {
+      const ship = this.ship;
+      const currentPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+      const nextPortIndex = currentPortIndex + 1;
+      const nextPortElement = document.querySelector(`[data-port-index='${nextPortIndex}']`);
+
+      if (!nextPortElement) {
+        return alert('End of the line!');
+      } else {
+        const shipElement = document.querySelector('#ship');
+        const sailInterval = setInterval(() => {
+          const shipLeft = parseInt(shipElement.style.left, 10);
+          if (shipLeft === (nextPortElement.offsetLeft - 32)) {
+            ship.setSail();
+            ship.dock();
+            clearInterval(sailInterval);
+          } else {
+            shipElement.style.left = `${shipLeft + 1}px`;
+          }
+        }, 20);
+      }
     }
   };
 
